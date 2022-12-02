8bdfb1d9426927a27591f40f1fa1a800488a12dd
Luiz Filipe <luizfilipemr17@gmail.com>
2022-12-02 20:42:06 -0300

    Banco de Dados atualiado

--- meubanco.sql
+++ meubanco.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01-Dez-2022 às 01:32
+-- Tempo de geração: 03-Dez-2022 às 00:41
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 7.4.29
 
@@ -55,6 +55,38 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`Id`, `username`, `usersPwd`) VALUES
+(7, 'Luiz', '$2y$10$3/.H52ilHmRZ9jggoAoOIuVeIGcIw7OoX/A50QPuhvAk4xFvWPDE.'),
+(8, 'Vitoria', '$2y$10$ZArGdfzQN3xySfHS8yx2MuFBSwCiXn0hgUQoHLUZse4NAEYEWEPVm'),
+(9, 'admin', '$2y$10$e6fI8ZYRwjfBWDJXWEb2auEYqyMBV.H6/zE1RB3tYQi3fYrmf6u2C');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usersimage`
+--
+
+CREATE TABLE `usersimage` (
+  `id` int(11) NOT NULL,
+  `urlImg` varchar(300) NOT NULL,
+  `albumName` varchar(128) NOT NULL,
+  `dateImg` date NOT NULL,
+  `userImg` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usersimage`
+--
+
+INSERT INTO `usersimage` (`id`, `urlImg`, `albumName`, `dateImg`, `userImg`) VALUES
+(5, 'https://i.pinimg.com/originals/7b/fc/79/7bfc792837b7edef3dc28b02721c78e9.jpg', 'Gatos', '2022-12-02', 'Vitoria'),
+(6, 'https://i.pinimg.com/originals/11/1a/03/111a03133d14214539c96e0f657dff1a.png', 'Paisagens', '2022-12-16', 'Vitoria'),
+(7, 'https://images.wallpaperscraft.com/image/single/astronaut_ring_neon_156673_1920x1080.jpg', 'Buraco Negro', '2022-10-04', 'Luiz');
+
+--
 -- Índices para tabelas despejadas
 --
 
@@ -71,6 +103,12 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`Id`);
 
 --
+-- Índices para tabela `usersimage`
+--
+ALTER TABLE `usersimage`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -84,7 +122,13 @@ ALTER TABLE `faleconosco`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de tabela `usersimage`
+--
+ALTER TABLE `usersimage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
