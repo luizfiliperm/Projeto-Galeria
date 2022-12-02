72535af6d7b5682fa58b92748a67ed8682695368
Luiz Filipe <luizfilipemr17@gmail.com>
2022-12-02 15:09:21 -0300

    Adcionando username dinamico no header

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     
-    include("admin/includes/config.inc.php");
+    include_once("admin/includes/config.inc.php");
 
     if(isset($_SESSION["username"])){
         include_once("templates/header.php");
