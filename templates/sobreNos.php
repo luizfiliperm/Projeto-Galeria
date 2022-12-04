78a9a2a25cd26dcdebc3e2791ae7c46de73fe179
Vitoria Berto <mvitoriaberto2@gmail.com>
2022-12-03 22:30:35 -0300

    Alteração de texto e imagenss

--- templates/sobreNos.php
+++ templates/sobreNos.php
@@ -5,11 +5,11 @@
                 <div class="row">
                     <div class="col-md-6 apply-box" id="company-img"></div>
                     <div class="col-md-6 apply-box" id="patern-img">
-                        <h4> Somos uma empresa mt seria</h4>
-                        <P>MEOW MEOW MEOW FOTOS FOTOS</P>
-                        <P>CLIQUE AQUI ABAIXO</P>
-                        <P>aqui tem gatinhos fofos</P>
-                        <a href="#" class="main-btn" id="apply-btn">Saiba mais</a>
+                        <h4> L&V Agency</h4>
+                        <P>Agencia formada com o intuito de criar novos projetos <br>trazendo o melhor para seus clientes e usuarios</P>
+                        <P>Projeto Php</P>
+                    
+                    
                     </div>
                 </div>
             </div>
@@ -19,44 +19,54 @@
          <!--Cards-->   
          <div class="cards-area">
             <div class="container">
-                <div class="row">
+                <div class="row ">
                     <div class="col-12">
                         <h3 class="main-title">Funcionarios</h3>
                     </div>
-                    <div class="col-md-3">
-                        <div class="card">
-                            <img src="templates/static/img/sobreNos/luiz.png" class="card-img-top"alt="">
+                    <div class="col-md-2 cards2 primeiro">
+                        <div class="card" style="width: 12rem;">
+                            <img src="templates/static/img/sobreNos/zigs.jpeg" class="card-img-top"alt="">
                             <div class="card-body">
-                                <a href="#" class="main-btn2"><h5 class="card-title">Luiz</h5></a>
-                                <p class="card-text"> nhame nhame </p>
+                                <a href="#" class="main-btn2"><h5 class="card-title">Ziggs</h5></a>
+                                <p class="card-text"> CEO </p>
                             
                             </div>
                         </div>
                     </div>  
-                    <div class="col-md-3">    
-                        <div class="card">
+                    <div class="col-md-2 cards2">
+                        <div class="card" style="width: 12rem;">
+                            <img src="templates/static/img/sobreNos/luiz.png" class="card-img-top"alt="">
+                            <div class="card-body">
+                                <a href="#" class="main-btn2"><h5 class="card-title">Luiz</h5></a>
+                                <p class="card-text"> developer </p>
+                            
+                            </div>
+                        </div>
+                    </div>  
+                    <div class="col-md-2 cards2">    
+                        <div class="card" style="width: 12rem;">
                             <img src="templates/static/img/sobreNos/vitoria.png" class="card-img-top"alt="">
                             <div class="card-body">
                                 <a href="#" class="main-btn2"><h5 class="card-title">Vitoria</h5></a>
-                                <p class="card-text"> nhame nhame </p>
+                                <p class="card-text"> developer </p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3">    
-                        <div class="card">
+                    <div class="col-md-2 cards2">    
+                        <div class="card" style="width: 12rem;">
                             <img src="templates/static/img/sobreNos/nylon.png" class="card-img-top"alt="">
                             <div class="card-body">
                                 <a href="#" class="main-btn2"><h5 class="card-title">Nylon</h5></a>
-                                <p class="card-text"> nhame nhame </p>
+                                <p class="card-text"> cão de apoio emocional </p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3">    
-                        <div class="card">
+                    <div class="col-md-2 cards2">    
+                        <div class="card" style="width: 12rem;">
                             <img src="templates/static/img/sobreNos/tiggo.png" class="card-img-top"alt="">
                             <div class="card-body">
                                 <a href="#" class="main-btn2"><h5 class="card-title">Tiggo</h5></a>
-                                <p class="card-text"> nhame nhame </p>
+                                <p class="card-text"> cão de apoio emocional </p>
                             </div>
                         </div>
                     </div>
